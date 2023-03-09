101954d007ef2db4d68d95802bd707031896f7ab
MalMWare <malkelleymartin@gmail.com>
2023-03-08 22:49:34 -0800

    complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
   return (
     <div className="App">
-      
+
     </div>
   );
 }
