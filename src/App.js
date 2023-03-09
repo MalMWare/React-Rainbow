ac3845fb8ef9bd9816cbf427b754aa79a9d3724d
MalMWare <malkelleymartin@gmail.com>
2023-03-08 23:34:46 -0800

    Completed react rainbow

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import ColorBlock from "./ColorBlock"
+import ColorForm from './ColorForm';
 
 function App() {
-  let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
+  let [colors, setColors] = useState( [ 
+   'violet', 'blue', 
+  'lightblue', 'green', 
+  'greenyellow', 'yellow', 
+  'orange', 'red'
+])
+  let colorMap = colors.map((color, i) => {
+    return (
+      <ColorBlock key={i} color={color} />
+    )
+  })
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
+  }
   return (
     <div className="App">
-
+       {colorMap}
+       <ColorForm addColor={addColor} />
     </div>
   );
 }
